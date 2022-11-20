92e95ea31f9a032860a0e7bb6dccd4a89364d976
cumong500 <khanhnau123@gmail.com>
2022-11-20 17:36:46 +0000

    complete proj

--- DevOps_Microservices/project-ml-microservice-kubernetes/upload_docker.sh
+++ DevOps_Microservices/project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="cumong500/proj4:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag proj4 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
